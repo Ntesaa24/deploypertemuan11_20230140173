6a0990c08d7c10236076b3cd988957cefd9beb68
Ntesaa24 <notesa.aldinasari24@gmail.com>
2026-05-18 14:45:26 +0700

    Menambahkan SecurityConfig.java

--- src/main/java/com/deploy/pertemuan11/security/SecurityConfig.java
+++ src/main/java/com/deploy/pertemuan11/security/SecurityConfig.java
@@ -1,4 +1,12 @@
 package com.deploy.pertemuan11.security;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 }
